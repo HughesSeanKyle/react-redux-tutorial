d9a3f5b038f6f8aaa7143eb92aaa86ae5e4d501d
HughesSeanKyle <khughessean@yahoo.com>
2020-12-30 14:20:29 +0200

    V235 - Named vs Default Exports

--- src/components/App.js
+++ src/components/App.js
@@ -5,3 +5,10 @@ const App = () => {
 };
 
 export default App;
+
+/*
+import { selectSong } from '../actions'; // 1
+
+If importing named export then use {}
+If importing default export then no need for  {}
+*/

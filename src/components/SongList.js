efb634d4e6caa5b9e66acb4cd535df7fd4a9dc35
HughesSeanKyle <khughessean@yahoo.com>
2020-12-31 14:14:22 +0200

    V243 - Redux is not magic - Delving deeper into the connect function and it's relation to dispatch function

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -30,28 +30,30 @@ class SongList extends Component {
 }
 
 const mapStateToProps = (state) => { // 1
-    console.log(state);
     return { songs: state.songs };
 };
 
 export default connect(
     mapStateToProps,
-    { selectSong }
-)(SongList); // 4
+    { selectSong } // 2
+)(SongList); 
 
 /*
-Called this by convention
-    We will take our state object. The data inside and we will run some computation on it. This will cause that data to evetually show up as props inside of the component.
+// 1 Called this by convention
+        We will take our state object. The data inside and we will run some computation on it. This will cause that data to evetually show up as props inside of the component.
     
 Will be called with all of the state inside redux store. It is the entire list of songs from the song reducer and the currently selected song from the selected song reducer. 
 
 We configure the connect function by passing it a function.
 
 Steps get data from redux store into a component using redux lib
-1. Import connect at the top
-2. Call connect at bottom and pass in component as the 2nd function call 
-3. Will always define mapStateToProps 
+a1. Import connect at the top
+a2. Call connect at bottom and pass in component as the 2nd function call 
+a3. Will always define mapStateToProps 
     mapStateToProps is always going to get 1st arg of state
     Will always return an obj that will show up as props inside component
-4. Can make this syntax shorter with some es6 if key and value is the same. Just use one word. selectSong: selectSong -> before refactor.  
+
+2. Can make this syntax shorter with some es6 if key and value is the same. Just use one word. selectSong: selectSong -> before refactor. 
+
+When this action creator is passed into connect function like this it is automatically passed to the dispatch function which then later updates the redux store with the action it has received. 
 */
